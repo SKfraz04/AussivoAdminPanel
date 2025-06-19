b87eb588ceca30bc9b05befc6097d77c7cc11983
SKfraz04 <Sakib.fraz@gmail.com>
2025-06-19 13:01:13 +0530

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Landing from "./pages/Landing";
-import Dashboard from "./pages/Dashboard";
-import Stake from "./pages/Stake";
-import Deposit from "./pages/Deposit";
-import ICO from "./pages/ICO";
-import Rewards from "./pages/Rewards";
-import Referrals from "./pages/Referrals";
-import Ranks from "./pages/Ranks";
-import Wallet from "./pages/Wallet";
 import AdminDashboard from "./pages/Admin";
 import NotFound from "./pages/NotFound";
 
@@ -26,17 +16,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/stake" element={<Stake />} />
-          <Route path="/deposit" element={<Deposit />} />
-          <Route path="/ico" element={<ICO />} />
-          <Route path="/rewards" element={<Rewards />} />
-          <Route path="/referrals" element={<Referrals />} />
-          <Route path="/ranks" element={<Ranks />} />
-          <Route path="/wallet" element={<Wallet />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/" element={<AdminDashboard />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
